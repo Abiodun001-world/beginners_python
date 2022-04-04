d155e6caec01db4f7f05ac7e990621f5186c2f88
Abiodun001-world <Adekunleabiodun354@gmail.com>
2022-04-05 00:18:34 +0100

    Basics python

---
+++ main.py
@@ -0,0 +1,7 @@
+print('Aboderin Emmanuel is a great person in my life')
+print('Python is a programming language')  
+print('I want to output line one')
+print("i want to output line two")
+print("This is another string")
+
+
